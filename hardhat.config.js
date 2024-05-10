34e0fe5359df7c962b54d2f026ebfb6907abde5b
ltphat2204 <ltphat2204@gmail.com>
2024-05-10 12:03:44 +0700

    Setup module and config

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,18 @@
 require("@nomicfoundation/hardhat-toolbox");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.24",
+  networks: {
+    polygon_amoy: {
+      url: process.env.POLYGON_NUMBAI,
+      accounts: [process.env.PRIVATE_KEY],
+    }
+  },
+  etherscan: {
+    apiKey: process.env.API_KEY,
+  }
 };
