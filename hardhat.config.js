1b5c77cf071218e1b4c9e08ad0e71743b0d5b5c0
ltphat2204 <ltphat2204@gmail.com>
2024-05-10 15:24:06 +0700

    Change setting to deploy to holesky

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,25 +7,13 @@ dotenv.config();
 module.exports = {
   solidity: "0.8.24",
   networks: {
-    polygon_amoy: {
-      url: process.env.POLYGON_RPC,
+    holesky: {
+      url: process.env.RPC,
       accounts: [process.env.PRIVATE_KEY],
     }
   },
   etherscan: {
-    apiKey: {
-      "polygon_amoy": process.env.API_KEY
-    },
-    customChains: [
-      {
-        network: "polygon_amoy",
-        chainId: 80002,
-        urls: {
-          apiURL: "https://api-amoy.polygonscan.com/api",
-          browserURL: "https://amoy.polygonscan.com/"
-        }
-      }
-    ]
+    apiKey: process.env.API_KEY
   },
   sourcify: {
     enabled: false
